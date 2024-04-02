14013a8dc1ef6d1c1e3e78fb210fdbd4fc97561a
Jens Desmet <57892373+smetsson@users.noreply.github.com>
2024-04-02 10:54:49 -0400

    Write first version of pseudocode

---
+++ js/javascript.js
@@ -0,0 +1,16 @@
+console.log("Hello World!")
+
+// Create variable for computerSelection
+// Create variable for playerSelection
+// Generate computer choice (rock, paper, scissors) and save in computerSelection
+// Get player choice (rock, paper, scissors -> case-insensitive) and save in playerChoice
+// Evaluate playerChoice versus computerChoice
+    // Same choice means tie
+    // Rock beats scissors
+    // Scissors beat paper
+    // Paper beats rock
+    // Show win or lose message
+// Create a gaming session that loops the Rock Paper Scissors game 5 times
+    // Keep score every loop
+    // Show score every loop
+    // Show winner or loser at the end of the 5th loop
