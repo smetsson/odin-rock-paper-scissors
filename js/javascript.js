3bf7329c88181ab79dcc674846a523d7815c9ac6
Jens Desmet <57892373+smetsson@users.noreply.github.com>
2024-04-02 11:39:02 -0400

    Create getComputerChoice function
    Created the getComputerChoice function twice to exercise:
    - getComputerChoice() -> implementation with if else statements
    - getComputerChoice2() -> implementation with ternary operator

--- js/javascript.js
+++ js/javascript.js
@@ -7,6 +7,26 @@ let computerSelection
 let playerSelection
 
 // Generate computer choice (rock, paper, scissors) and save in computerSelection
+function getComputerChoice() {
+    let i = Math.random()
+    console.log(i);
+    if (i < 0.333) {
+        return "rock";
+    } else if ( i >= 0.333 && i <= 0.666) {
+        return "paper";
+    } else {
+    return "scissors";
+    }
+}
+
+function getComputerChoice2() {
+    let i = Math.random()
+    console.log(i);
+    return (i < 0.333) ? "rock" 
+    : (i >= 0.333 && i <= 0.666) ? "paper"
+    : "scissors";
+}
+
 // Get player choice (rock, paper, scissors -> case-insensitive) and save in playerChoice
 // Evaluate playerChoice versus computerChoice
     // Same choice means tie
